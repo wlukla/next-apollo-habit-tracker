39efb377fc4f9aee0dfb387e6c166cdebe150a46
wlukla <vladislav.danilochkin@gmail.com>
2020-07-25 18:00:35 +0300

    feat: set up apollo server

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,9 +1,25 @@
-export default (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.statusCode = 200;
-  res.end(
-    JSON.stringify({
-      test: 'Hello next api',
-    }),
-  );
+import { ApolloServer, gql } from 'apollo-server-micro';
+
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHello: () => {
+      return 'Hello apollo server';
+    },
+  },
 };
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default apolloServer.createHandler({ path: '/api/graphql' });
